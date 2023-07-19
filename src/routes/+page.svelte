86fc8326bba1433f027085dacefd084d67cc87ce
rts4 <mapfg.rts4@techhh.ca>
2023-07-18 22:28:02 -0400

    finish first site concept - rts4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
+<script lang="ts">
+    // Route: {name: string, href: string}
+    const routes = [
+        { name: "Elements", href: "https://element.techhh.ca" },
+        { name: "Quadratics", href: "https://quadratics.techhh.ca" }
+    ];
+</script>
+
 <div class="flex h-screen place-items-center">
-<div class="container mx-auto p-5 my-5 text-white rounded-3xl grid justify-center" id="main-container">
-    <h1 class="text-2xl quicksand-reg">Whereas disregard and contempt for human rights have resulted.</h1>
-</div>
+    <div class="container mx-auto p-5 my-5 text-white rounded-3xl flex flex-wrap justify-center justify-items-center" id="main-container">
+        <h1 class="text-2xl quicksand-reg">rts4</h1>
+        <p class="quicksand-reg w-full text-center p-1">Below is a list of my previous or on-going projects.</p>
+        {#each routes as route}
+            <a href={route.href} class="p-2 my-1 rounded-3xl shadow-2xl border border-gray-400 w-full text-center" id="button">{route.name}</a>
+        {/each}
+        <div class="w-full justify-center p-2">
+            <p class="quicksand-reg">Follow me on <a href="https://github.com/rts4" class="underline font-bold">GitHub</a>.</p>
+        </div>
+    </div>
 </div>
 
 <style lang="postcss">
@@ -10,6 +25,12 @@
     }
     #main-container {
         background-color: rgb(30, 30, 30);
+    }
+    #button {
+        background-color: rgb(20, 20, 20);
+    }
+    #button:hover {
+        background-color: rgb(10, 10, 10);
     }
     .quicksand-reg {
         font-family: 'Quicksand', sans-serif;
