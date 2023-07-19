1536faec436763a1f0ed5e7117d25d504b79eb27
rts4 <mapfg.rts4@techhh.ca>
2023-07-18 21:14:42 -0400

    install quicksand font - rts4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
-<p class="font-bold underline text-white">man</p>
+<div class="flex h-screen place-items-center">
+<div class="container mx-auto p-5 my-5 text-white rounded-3xl grid justify-center" id="main-container">
+    <h1 class="text-2xl quicksand-reg">Whereas disregard and contempt for human rights have resulted.</h1>
+</div>
+</div>
 
 <style lang="postcss">
     :global(html) {
-        background-color: theme(colors.gray.500);
+        background-color: rgb(15, 15, 15);
+    }
+    #main-container {
+        background-color: rgb(30, 30, 30);
+    }
+    .quicksand-reg {
+        font-family: 'Quicksand', sans-serif;
+        font-weight: 400;
     }
 </style>
