ec36bae2cff3711340a45bbe736c63e6e0af4588
rts4 <mapfg.rts4@techhh.ca>
2023-07-18 20:34:36 -0400

    configure tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="font-bold underline text-white">man</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.500);
+    }
+</style>
